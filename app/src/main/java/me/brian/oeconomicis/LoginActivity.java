8b330f07a4913ccabbf0ca403aeee4d0e06e5fce
rzkbrian <rzknairb@gmail.com>
2019-07-05 18:08:01 -0500

    splash screen

---
+++ app/src/main/java/me/brian/oeconomicis/LoginActivity.java
@@ -0,0 +1,15 @@
+package me.brian.oeconomicis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
